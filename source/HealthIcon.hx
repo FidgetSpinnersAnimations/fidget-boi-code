1b171841e985c5ff1ef5b6f82b1b54afc0ef8ef9
FidgetSpinnersAnimations <aalmanfi5@gmail.com>
2021-08-16 19:22:46 -0500

    Initial commit

---
+++ source/HealthIcon.hx
@@ -0,0 +1,50 @@
+package;
+
+import flixel.FlxSprite;
+
+class HealthIcon extends FlxSprite
+{
+	/**
+	 * Used for FreeplayState! If you use it elsewhere, prob gonna annoying
+	 */
+	public var sprTracker:FlxSprite;
+
+	public function new(char:String = 'bf', isPlayer:Bool = false)
+	{
+		super();
+		
+		loadGraphic(Paths.image('iconGrid'), true, 150, 150);
+
+		antialiasing = true;
+		animation.add('bf', [0, 1], 0, false, isPlayer);
+		animation.add('fidget-spinner', [2, 3], 0, false, isPlayer);
+		animation.add('lamp', [4, 5], 0, false, isPlayer);
+		animation.add('lamp-angry', [6, 7], 0, false, isPlayer);
+		animation.add('fidget-lamp', [8, 9], 0, false, isPlayer);
+		animation.add('fidget-lamp-angry', [10, 11], 0, false, isPlayer);
+		animation.add('sus-boy', [12, 13], 0, false, isPlayer);
+        animation.add('sad-boy', [14, 14], 0, false, isPlayer);
+        animation.add('him', [15, 15], 0, false, isPlayer);
+		animation.add('dad', [16, 17], 0, false, isPlayer);
+		animation.add('spooky', [18, 19], 0, false, isPlayer);
+		animation.add('pico', [20, 21], 0, false, isPlayer);
+		animation.add('mom', [22, 23], 0, false, isPlayer);
+		animation.play(char);
+
+		switch(char)
+		{
+			case 'bf-pixel':
+				antialiasing = false;
+		}
+
+		scrollFactor.set();
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (sprTracker != null)
+			setPosition(sprTracker.x + sprTracker.width + 10, sprTracker.y - 30);
+	}
+}
